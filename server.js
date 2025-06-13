63553132b798eb8048da250e3b1090e27982935e
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-06-13 18:26:13 -0300

    fix db

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const jsonServer = require("json-server");
-// const auth = require("json-server-auth");
+const auth = require("json-server-auth");
 const cors = require("cors");
 const port = process.env.PORT || 3001;
-// const routes = require('./routes.json');
+const routes = require('./routes.json');
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 app.db = router.db;
 
-// const rules = auth.rewriter(routes);
+const rules = auth.rewriter(routes);
 
 app.use(cors());
-// app.use(rules);
-// app.use(auth);
+app.use(rules);
+app.use(auth);
 app.use(router);
 app.listen(port);
 
